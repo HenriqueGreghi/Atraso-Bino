7f46a38cf714a087266773839748f5d16acd5601
Henrique Greghi <henriquevsg@gmail.com>
2022-08-28 13:59:31 -0300

    Criando calculo de atraso do Bino

---
+++ AtrasoBino/Calculo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtrasoBino
+{
+    class Calculo
+    {
+        public int Hora { get; set; }
+        public int Minuto { get; set; }
+
+        public Calculo(int hora, int minuto)
+        {
+            Hora = hora;
+            Minuto = minuto;
+        }
+        public string Atraso()
+        {
+            if (Hora < 7)
+            {
+                return "Não se atrasou = 00:00";
+            }
+            else if (Hora == 7)
+            {
+                return "Se atrasou = 00:" + Minuto;
+            }
+            else
+            {
+                return "Se atrasou = " + (Hora - 7 + 1) + ":" + Minuto;
+            }
+        }
+    }
+}
---
+++ AtrasoBino/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AtrasoBino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            Console.WriteLine("Qual o horário que o Bino acordou?");
+            string[] valores = Console.ReadLine().Split(':');
+            int hora = int.Parse(valores[0]);
+            int minuto = int.Parse(valores[1]);
+
+            Calculo calc = new Calculo(hora, minuto);
+
+            Console.WriteLine(calc.Atraso());
+        }
+    }
+}
